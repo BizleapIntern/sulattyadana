5d1d845c881cd089b88103aae405006dd932ada2
BizleapIntern <bizleapinternship@gmail.com>
2018-05-04 10:56:42 +0630

    tst

---
+++ Git/src/com/bizleap/training/git/Git.java
@@ -0,0 +1,18 @@
+package com.bizleap.training.git;
+
+public class Git {
+	public static void main(String[] args) {
+  		int res=0;
+		int i;
+		for(i=1;i<=20;i++)
+		{
+			res=res+i;
+			System.out.println( i +"="+res);
+		}
+		
+		
+
+	}
+
+
+}
